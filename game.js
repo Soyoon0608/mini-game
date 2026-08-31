d60273e9b16f88a30a328d8bdad50d35aa20e23c
Soyoon0608 <kdh68686@naver.com>
2026-08-31 17:25:05 +0900

    Update game.js

--- game.js
+++ game.js
@@ -19,7 +19,7 @@ let totalPausedTime = 0;
 const gameTime = 20000;
 
 /* 과제 3 난이도 값 */
-const OBSTACLE_SPEED = 7;
+const OBSTACLE_SPEED = 10;
 
 function getMaxPlayerX() {
     return game.clientWidth - player.offsetWidth;
